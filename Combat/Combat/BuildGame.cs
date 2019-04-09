6e3accf2c1b962ea840796f5f8ce5f7e88c25a62
MAchtman94 <machtman@umich.edu>
2019-04-09 11:36:32 -0400

    Updated Tank names and filled out rectangles

--- Combat/Combat/BuildGame.cs
+++ Combat/Combat/BuildGame.cs
@@ -14,9 +14,9 @@ namespace Combat
         private Tank playerTank;
         private Tank otherTank;
         private Points pointsPlayer;
-        private Points pointsComputer;
+        private Points pointsOther;
         private Bullets playerBullets;
-        private Bullets computerBullets;
+        private Bullets otherBullets;
         private List<Walls> walls;
         private List<IDrawable> drawables;
         private List<ICollidable> collidables;
@@ -26,10 +26,10 @@ namespace Combat
             drawables = new List<IDrawable>();
             walls = new List<Walls>();
             collidables = new List<ICollidable>();
-            playerTank = new Tank(5, 5, 5, 5, Colors.Black);
-            otherTank = new Tank(5, 6, 6, 6, Colors.Blue);
+            playerTank = new Tank(30, 30, 100, 100, Colors.Black);
+            otherTank = new Tank(300, 300, 60, 60, Colors.Blue);
             pointsPlayer = new Points();
-            pointsComputer = new Points();
+            pointsOther = new Points();
             playerBullets = new Bullets(20, 20, 5, 5, Colors.Blue);
 
             //Automatically placing the bullets to move towards the right on X-Axis
@@ -37,14 +37,12 @@ namespace Combat
             playerBullets.TravelingUpward = false;
             playerBullets.TravelingDownward = false;
 
-            computerBullets = new Bullets(900, 600, 5, 5, Colors.Orange);
+            otherBullets = new Bullets(900, 600, 5, 5, Colors.Orange);
 
             //Automatically placing the bullets to move towards the left on X-Axis
-            computerBullets.TravelingLeftWard = true;
-            computerBullets.TravelingUpward = false;
-            computerBullets.TravelingDownward = false;
-
-            //playerTank = new Tank(100, 100, 50, 50, Colors.Blue);
+            otherBullets.TravelingLeftWard = true;
+            otherBullets.TravelingUpward = false;
+            otherBullets.TravelingDownward = false;
 
             //Boundary of game
             var outsideWall = new Walls(10,10,1000,700, Colors.Black);
@@ -57,17 +55,18 @@ namespace Combat
 
             //Adding bullets
             drawables.Add(playerBullets);
-            drawables.Add(computerBullets);
+            drawables.Add(otherBullets);
 
 
 
-            //drawables.Add(playerTank);
+            drawables.Add(playerTank);
+            drawables.Add(otherTank);
         }
 
         public void Update()
         {
             playerBullets.Update();
-            computerBullets.Update();
+            otherBullets.Update();
         }
 
         public void DrawGame(CanvasDrawingSession canvas)
@@ -103,7 +102,7 @@ namespace Combat
                 Y = y;
                 Height = height;
                 Width = width;
-                color = Colors;
+                Colors = color;
             }
 
             public bool Collides(int x, int y, int height, int width)
@@ -113,7 +112,7 @@ namespace Combat
 
             public void Draw(CanvasDrawingSession canvas)
             {
-                //canvas.DrawEllipse(X, Y, Height, Width, Colors, 3);
+                canvas.FillRectangle(X, Y, Height, Width, Colors);
             }
         }
 
@@ -192,6 +191,10 @@ namespace Combat
             public bool TravelingLeftWard { get; set; }
             public bool TravelingUpward { get; set; }
 
+            //Looking at a diagonal view, only use in Y-Axis change views
+            public bool DiagnolTravelRight { get; set; }
+            public bool DiagnolTravelLeft { get; set; }
+
             public Bullets(int x, int y, int height, int width, Color color)
             {
                 X = x;
@@ -206,10 +209,31 @@ namespace Combat
                 //Determine what position user is going.  Bullets can only go in one direction
                 if (TravelingDownward)
                 {
+                    if (DiagnolTravelLeft)
+                    {
+                        Y += 1;
+                        X -= 1;
+                    }
+                    else if (DiagnolTravelRight)
+                    {
+                        Y += 1;
+                        X += 1;
+                    }
                     Y += 1;
                 }
                 else if (TravelingUpward)
                 {
+                    if (DiagnolTravelLeft)
+                    {
+                        Y -= 1;
+                        X -= 1;
+       
+                    }
+                    else if (DiagnolTravelRight)
+                    {
+                        Y -= 1;
+                        X += 1;
+                    }
                     Y -= 1;
                 }
                 else if (TravelingLeftWard)
@@ -224,12 +248,12 @@ namespace Combat
 
             public bool Collides(int x, int y, int height, int width)
             {
-                //return x >= X && x <= Width && y >= Y && y <= Height;
+                return x >= X && x <= Width && y >= Y && y <= Height;
             }
 
             public void Draw(CanvasDrawingSession canvas)
             {
-                canvas.DrawRectangle(X, Y, Height, Width, Color);
+                canvas.FillRectangle(X, Y, Height, Width, Color);
             }
         }
 
